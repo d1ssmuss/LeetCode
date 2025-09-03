86a6c95bf099ee04a09ff589dcc950fea31f9cd0
Линар <159605589+d1ssmuss@users.noreply.github.com>
2025-09-03 21:14:14 +0400

    Add files via upload

---
+++ 169. Majority Element.py
@@ -0,0 +1,7 @@
+def majorityElement(nums):
+    _set = set(nums) # O(1)
+    for i in _set:
+        if nums.count(i) > len(nums) / 2:
+            return i
+
+print(majorityElement([2,2,1,1,1,2,2]))
