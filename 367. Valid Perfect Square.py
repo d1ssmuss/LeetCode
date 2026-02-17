af576f0e0751fd0fffe49fa3971c6a29f389a12c
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-02-17 19:31:49 +0400

    Update 367. Valid Perfect Square.py

--- 367. Valid Perfect Square.py
+++ 367. Valid Perfect Square.py
@@ -1,13 +1,11 @@
-class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        if num == 1:
-            return True
-        i = 2
-        while i * i <= num:
-            if i * i == num:
-                return True
-                break
-            else:
-                i += 1
-        else:
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        x = num ** 0.5
+        if x - int(x) != 0:
             return False
+        else:
+            return True
