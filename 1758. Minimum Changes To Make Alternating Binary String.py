81ed9b4fc222adbd63a1c72e98e472c7491c9023
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-03-07 23:57:08 +0400

    Add files via upload

--- 1758. Minimum Changes To Make Alternating Binary String.py
+++ 1758. Minimum Changes To Make Alternating Binary String.py
@@ -1,30 +1,22 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        nums_1 = [int(i) for i in s]
-        nums_2 = [int(i) for i in s]
         count_1 = 0
         count_2 = 0
-        for i in range(len(nums_1) - 1):
-            if nums_1[i] == nums_1[i + 1]:
-                if nums_1[i] == 0:
-                    nums_1[i + 1] = 1
+        n = len(s)
+        for i in range(n):
+            if i % 2 == 0:
+                if s[i] == '0':
+                    count_1 += 1
                 else:
-                    nums_1[i + 1] = 0
-                count_1 += 1
-        if nums_2[0] == 1:
-            nums_2[0] = 0
-        else:
-            nums_2[0] = 1
-        count_2 += 1
-        for i in range(len(nums_2) - 1):
-            if nums_2[i] == nums_2[i + 1]:
-                if nums_2[i] == 0:
-                    nums_2[i + 1] = 1
+                    count_2 += 1
+            else:
+                if s[i] == '1':
+                    count_1 += 1
                 else:
-                    nums_2[i + 1] = 0
-                count_2 += 1
-        return min(count_1, count_2)
-        # return nums, count
+                    count_2 += 1
+        return count_1, count_2, min(count_1, count_2)
+
+
 
 print(Solution().minOperations("1111")) # 2
 print(Solution().minOperations("10")) # 0
