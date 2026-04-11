c75e062175033c255fcacffecb16cff03a5e2cf6
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-04-11 06:37:41 +0400

    Do Better

---
+++ 3741. Minimum Distance Between Three Equal Elements II.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for index, value in enumerate(nums):
+            if value not in d:
+                d[value] = []
+            d[value].append(index)
+        print(d)
+        mn = float('+inf')
+        for spisok in d.values():
+            if len(spisok) >= 3:
+                for index in range(len(spisok) - 2):
+                    troika = spisok[index:index + 3]
+                    # print(troika)
+                    i = troika[0]
+                    j = troika[1]
+                    k = troika[2]
+                    rs = abs(i - j) + abs(j - k) + abs(k - i)
+                    mn = min(mn, rs)
+        return mn if mn != float('+inf') else -1
