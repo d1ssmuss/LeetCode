af3a2b9c39aee47e0d783d47b149e4b23367656e
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-04-21 17:03:28 +0400

    Update 509. Fibonacci Number.py

--- 509. Fibonacci Number.py
+++ 509. Fibonacci Number.py
@@ -10,3 +10,18 @@ class Solution(object):
             return 1
         else:
             return self.fib(n - 1) + self.fib(n - 2)
+
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        f = []
+        for i in range(n + 1):
+            if i <= 1:
+                f.append(i)
+            else:
+                f.append(f[i - 1] + f[i - 2])
+        return f[n]
