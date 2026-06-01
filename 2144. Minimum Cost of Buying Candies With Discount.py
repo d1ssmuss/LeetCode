736debf83936c447af6af3f943549de8c78516ea
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-06-01 10:48:31 +0400

    Add files via upload

---
+++ 2144. Minimum Cost of Buying Candies With Discount.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minimumCost(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        cost.sort(reverse = True)
+        s = 0
+        for i in range(len(cost)):
+            if i % 3 != 2:
+                s += cost[i]
+        return s
+
+
+
+
+print(Solution().minimumCost([1,2,3]))
+print(Solution().minimumCost([6,5,7,9,2,2]))
+print(Solution().minimumCost([5,5]))
