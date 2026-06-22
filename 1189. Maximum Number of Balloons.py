3c1c1fc8f50db5217b440a1adeb776835d954844
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-06-22 15:57:13 +0400

    Update 1189. Maximum Number of Balloons.py

--- 1189. Maximum Number of Balloons.py
+++ 1189. Maximum Number of Balloons.py
@@ -4,20 +4,29 @@ class Solution(object):
         :type text: str
         :rtype: int
         """
-        letters = {'b': 0, 'a':0, 'l_1':0, 'l_2':0, 'o_1':0, 'o_2':0, 'n':0}
-        for letter in text:
-            if letter in 'ban':
-                letters[letter] += 1
-            elif letter == 'l':
-                if letters['l_1'] != letters['l_2']:
-                    letters['l_2'] += 1
-                else:
-                    letters['l_1'] += 1
-            elif letter == 'o':
-                if letters['o_1'] != letters['o_2']:
-                    letters['o_2'] += 1
-                else:
-                    letters['o_1'] += 1
-        return min(letters.values())
-        убираем лишние чёрные буквы
-        len(answ) / len(set(answ))
+        # 0 ms
+        # d = {}
+        # d['b'] = text.count('b')
+        # d['a'] = text.count('a')
+        # d['l_1'] = text.count('l') // 2
+        # d['l_2'] = text.count('l') - d['l_1']
+        # d['o_1'] = text.count('o') // 2
+        # d['o_2'] = text.count('o') - d['o_1']
+        # d['n'] = text.count('n')
+        # return d, d.values(), min(d.values())
+
+        d = {i : 0 for i in 'balon'}
+        for i in text:
+            if i in 'balon':
+                d[i] += 1
+        d['l'] = d['l'] // 2
+        d['o'] = d['o'] // 2
+        return min(d.values())
+
+
+
+print(Solution().maxNumberOfBalloons("nlaebolko"))
+print(Solution().maxNumberOfBalloons("loonbalxballpoon"))
+print(Solution().maxNumberOfBalloons("leetcode"))
+print(Solution().maxNumberOfBalloons("balon"))
+print(Solution().maxNumberOfBalloons("krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw"))
