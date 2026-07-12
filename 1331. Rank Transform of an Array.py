8bcfd2a82d9bb732debe5b141fa4b0a2d45ed19e
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-07-12 06:25:07 +0400

    Update 1331. Rank Transform of an Array.py

--- 1331. Rank Transform of an Array.py
+++ 1331. Rank Transform of an Array.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        sorted_arr = sorted(set(arr))
-        d = {j:i for i,j in enumerate(sorted_arr, start=1)}
+        array = sorted(set(arr))
+        d = {array[i]:i+1 for i in range(len(array))}
+        # print(array, d)
         return [d[i] for i in arr]
+        # sorted_arr = sorted(set(arr))
+        # d = {j:i for i,j in enumerate(sorted_arr, start=1)}
+        # return [d[i] for i in arr]
