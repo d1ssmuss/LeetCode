a78ca8ef568748f77a19efae8a639a53d792ea31
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-08-29 14:09:18 +0400

    Create 860. Lemonade Change.py

---
+++ 860. Lemonade Change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+        for cost in bills:
+            if cost == 5:
+                five += 1
+            elif cost == 10:
+                if five > 0:
+                    five -= 1
+                    ten += 1
+                else:
+                    return False
+            else:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        else:
+            return True
