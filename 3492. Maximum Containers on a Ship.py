7470c9ae4cd5dc67f2dab48f305a7ffb200c0290
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-09-21 00:49:16 +0400

    Create 3492. Maximum Containers on a Ship.py

---
+++ 3492. Maximum Containers on a Ship.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxContainers(self, n, w, maxWeight):
+        """
+        :type n: int
+        :type w: int
+        :type maxWeight: int
+        :rtype: int
+        """
+        return maxWeight // w if (maxWeight // w) <= n ** 2 else n ** 2
