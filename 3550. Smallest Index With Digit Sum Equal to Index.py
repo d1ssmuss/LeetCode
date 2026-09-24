93ddc91710b06ed354a90ea13e78eee591e65c01
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-09-24 08:53:24 +0400

    Update 3550. Smallest Index With Digit Sum Equal to Index.py (4ms)

--- 3550. Smallest Index With Digit Sum Equal to Index.py
+++ 3550. Smallest Index With Digit Sum Equal to Index.py
@@ -1,11 +1,26 @@
+# class Solution(object):
+#     def smallestIndex(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         for num in range(len(nums)):
+#             n = sum(list([int(i) for i in str(nums[num])]))
+#             if n == num:
+#                 return num
+#         return -1
 class Solution(object):
     def smallestIndex(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        for num in range(len(nums)):
-            n = sum(list([int(i) for i in str(nums[num])]))
-            if n == num:
-                return num
+        n = len(nums)
+        for i in range(n):
+            s = 0
+            while nums[i] != 0:
+                s += nums[i] % 10
+                nums[i] //= 10
+            if s == i:
+                return i
         return -1
