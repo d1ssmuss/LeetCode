2b4cdd78b95287cef2e4f7fa09adb491259230db
Линар <159605589+d1ssmuss@users.noreply.github.com>
2026-09-25 08:08:09 +0400

    Create 1025. Divisor Game.py

---
+++ 1025. Divisor Game.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # What's the strategy ?
+        return True if n % 2 == 0 else False
